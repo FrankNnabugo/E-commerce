1f81e05de0f9d61da55d67ca530d7a216f99feff
FrankNnabugo <franksamuel224@gmail.com>
2023-12-30 03:43:22 -0800

    mail config

---
+++ utils/sendMail.js
@@ -0,0 +1,17 @@
+const{transporter} = require("../../utils/nodemailer");
+
+const sendMail = async(message)=>{
+    try{
+
+        const mail = await transporter.sendMail(message);
+        res.status(200).json({mssg: " password reset token sent to user"})
+    }
+
+    catch(err){
+        console.err("err:", err)
+        res.status(500).send("error occured while sending mail");
+    }
+}
+
+
+module.exports = {sendMail};
